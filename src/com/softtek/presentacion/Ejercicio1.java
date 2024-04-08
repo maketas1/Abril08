b43f5cb2ce4fbd1d6ecf9ee22da5250d0b28dd5f
Ángel Gallardo <angelgallardoturpin@gmail.com>
2024-04-08 09:52:55 +0200

    Initial commit

---
+++ src/com/softtek/presentacion/Ejercicio1.java
@@ -0,0 +1,11 @@
+package com.softtek.presentacion;
+
+import java.lang.ref.SoftReference;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Apartado 1");
+        System.out.println("Lo que ocurre es que ");
+    }
+}
